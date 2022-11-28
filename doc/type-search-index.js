0b306dd4ad00df62cc4ff85297b7402cc4ecd504
SnewZz <64595565+SnewZz@users.noreply.github.com>
2022-11-28 17:26:35 +0100

    add of the javadoc

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"LexicalUnit"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Parser"},{"p":"<Unnamed>","l":"ParseTree"},{"p":"<Unnamed>","l":"Symbol"},{"p":"<Unnamed>","l":"TexHandler"}]
